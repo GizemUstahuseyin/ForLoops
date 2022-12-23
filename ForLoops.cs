3f99b592895ad87fb93b3705241e150dcc86bfba
Gizem <gizemustahuseyin123@gmail.com>
2022-12-23 22:28:58 +0300

    Add files via upload

---
+++ ForLoops.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class ForLoops
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            //int i=1 '1den başla)
+            //i <=100 (i100den küçük eşit olsun yani 100e kadar yazsın)
+            //i++ (i'yi bir arttır)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Finished!");
+            Console.ReadLine();
+        }
+    }
+}
